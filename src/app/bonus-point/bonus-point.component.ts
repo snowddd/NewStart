71c5fa150fc2c0d8a4867c7be67cfed9a0058796
snow <snowmaos@gmail.com>
2022-08-01 04:29:48 +0800

    feat: bonus point rxjsAns

--- src/app/bonus-point/bonus-point.component.ts
+++ src/app/bonus-point/bonus-point.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BsJs1Service } from './bs-js1.service';
-import { of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { from, of } from 'rxjs';
+import { concatMap, delay, mergeMap, switchMap, tap, skip } from 'rxjs/operators';
 
 @Component({
   selector: 'app-bonus-point',
@@ -9,8 +9,8 @@ import { tap } from 'rxjs/operators';
   styleUrls: ['./bonus-point.component.css']
 })
 export class BonusPointComponent implements OnInit {
-  
-  q1Data = ['F1','F2','F3','F4','','F6','F7','F8','F9']
+
+  q1Data = ['F1', 'F2', 'F3', 'F4', '', 'F6', 'F7', 'F8', 'F9']
 
   constructor(
     private bsJs1Service: BsJs1Service,
@@ -32,13 +32,31 @@ export class BonusPointComponent implements OnInit {
   }
 
   answerCookie1() {
+    document.cookie = "Key=ck1; path=/";
     //ToDo..
   }
 
   answerRxjs1() {
-    const first$ = of('first' ,  2000).pipe(tap(() => console.log('first')));
+    const first$ = of('first', 2000).pipe(tap(() => console.log('first')));
     const second$ = of('second', 1500).pipe(tap(() => console.log('second')));;
     const third$ = of('thrid', 800).pipe(tap(() => console.log('thrid')));;
+
+    let temp = console.log;
+    from([first$, second$, third$])
+      .pipe(
+        concatMap((ob) =>
+          ob.pipe(switchMap((v) => {
+            if (!!Number(v)) {
+              console.log = temp;
+              return of(v);
+            } else {
+              console.log = () => {};
+              return of();
+            }
+          }))
+        )
+      ).subscribe();
+
 
     // 考題限制 : 執行順序需為 : first$ => second$ => third$
     // 預期的Console結果 :
@@ -56,13 +74,13 @@ export class BonusPointComponent implements OnInit {
     this.bsJs1Service.getSample()
       .forEachChilds((child) => {
         // ToDo : 實作你的解決方案...
-        if(newResult.find((r)=>r === child.parent.value)){
+        if (newResult.find((r) => r === child.parent.value)) {
           newResult.pop();
         }
         newResult.push(child.value);
         newResult.push(child.parent.value);
       });
-      alert(newResult.join(' ,'));
+    alert(newResult.join(' ,'));
 
     // 預期alert的結果 => js 1 answer : child_1_1 , child_1_2 , parent_1 , child_2_1 ,  parent_2 ,child_3_1 , child_3_2 , child_3_3 , parent_3
     // alert(`js 1 answer : ${result}`)
